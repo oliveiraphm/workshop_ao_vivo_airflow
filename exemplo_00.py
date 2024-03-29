39dfa780ff41a463774e00bbe401cfe105957802
oliveiraphm <oliveiraphm@gmail.com>
2024-03-29 18:25:05 -0300

    third commit

---
+++ exemplo_00.py
@@ -0,0 +1,24 @@
+
+from time import sleep
+
+
+def primeira_atividade():
+    print("minha primeira atividade!")
+    sleep(2)
+
+def segunda_atividade():
+    print("minha segunda atividade!")
+    sleep(2)
+
+def terceira_atividade():
+    print("minha terceira atividade!")
+    sleep(2)
+
+def pipeline():
+    primeira_atividade()
+    segunda_atividade()
+    terceira_atividade()
+    print("pipeline finalizou!")
+    
+if __name__ == "__main__":
+    pipeline()
